71c4a66c192e5f38551ba2667de35db5bdc78f7a
Twinny VR <dev@twinnyvr.com.br>
2026-01-13 14:09:30 -0300

    SpatialAnchorDebug Label added

--- Runtime/Scripts/UI/AnchorDebug.cs
+++ Runtime/Scripts/UI/AnchorDebug.cs
@@ -7,11 +7,13 @@ using Concept.SmartTools;
 
 namespace Twinny.UI
 {
+    [ExecuteInEditMode]
     public class AnchorDebug : MonoBehaviour
     {
         private Transform _transform;
         [SerializeField] private GameObject _debugVisual;
 
+        [SerializeField]
         private string _displayText;
         public string displayText { get => _displayText; set => _displayText = value;  }
 
@@ -21,15 +23,25 @@ namespace Twinny.UI
         [SerializeField] private GameObject _debugInfo;
         [SerializeField] private TextMeshProUGUI TMP_Info;
 
+#if UNITY_EDITOR
+        private void OnValidate()
+        {
+            _debugInfo?.SetActive(_showInfo);
+        }
+#endif
+
         // Start is called before the first frame update
         void Start()
         {
-            Debug.LogWarning("[AnchorDebug] Active:" + (TwinnyRuntime.GetInstance<TwinnyXRRuntime>().ambientType != BuildType.RELEASE));
-            _debugVisual.SetActive(TwinnyRuntime.GetInstance<TwinnyXRRuntime>().ambientType != BuildType.RELEASE);
-            _transform = transform;
-            _debugInfo.SetActive(_showInfo);
-            if(_showInfo )
-            StartCoroutine(SetCoordinatesText());
+            //  Debug.LogWarning("[AnchorDebug] Active:" + (TwinnyRuntime.GetInstance<TwinnyXRRuntime>().ambientType != BuildType.RELEASE));
+#if UNITY_EDITOR
+            if (!Application.isPlaying)
+            {
+                SetDebugVisual(true);
+                return;
+            } 
+#endif
+            SetDebugVisual(TwinnyRuntime.GetInstance<TwinnyXRRuntime>().ambientType != BuildType.RELEASE);
         }
 
         // Update is called once per frame
@@ -38,12 +50,20 @@ namespace Twinny.UI
 
         }
 
+        public void SetDebugVisual(bool status)
+        {
+            _debugVisual?.SetActive(status);
+            _transform = transform;
+            _debugInfo?.SetActive(_showInfo);
+            if (_showInfo)
+                StartCoroutine(SetCoordinatesText());
+        }
 
         IEnumerator SetCoordinatesText()
         {
             while (_showInfo)
             {
-                TMP_Info.text = $"[{_displayText}]\nP:({_transform.position})\nR:({_transform.eulerAngles})"; 
+                TMP_Info.text = $"<b><color=#0F0>[{_displayText}]</color></b>\nP:({_transform.position})\nR:({_transform.eulerAngles})"; 
 
                 yield return new WaitForSeconds(1f);
             }
